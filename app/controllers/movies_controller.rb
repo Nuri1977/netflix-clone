f359a04e9a742baf4bf80a808209d1a0d547691c
Nuri1977 <nurilacka@gmail.com>
2021-12-18 01:37:27 +0100

    Add authentication with devise gem

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @movies = Movie.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "/home", to: "pages#home"
   # get "/movies", to: "movies#index"
   resources :movies
